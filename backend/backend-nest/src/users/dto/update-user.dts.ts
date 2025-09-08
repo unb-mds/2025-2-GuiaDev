efab0e941ad487877392357e0e1f3cbf5b7ae2b6
Hugo Freitas Silva <videogame041@gmail.com>
2025-09-08 20:31:40 -0300

    fix: comenta dto de objeto inexistente

--- backend/backend-nest/src/users/dto/update-user.dts.ts
+++ backend/backend-nest/src/users/dto/update-user.dts.ts
@@ -1,2 +1,4 @@
+// import { CreateUserDto } from "./create-user.dto"
+// import { PartialType } from "./@nestjs/mapped-typpes" 
 import { CreateUserDto } from "./create-user.dto"
-import { PartialType } from "./@nestjs/mapped-typpes" 
+import { PartialType } from "@nestjs/mapped-types";
