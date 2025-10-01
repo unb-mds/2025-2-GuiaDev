a99ebacf86f0951798d2654093532567b422dd76
filipe brito <filipebritogomesramos@gmail.com>
2025-10-01 11:16:17 -0300

    feature: autenticação de login e token JWT

--- backend/backend-nest/src/modules/auth/auth.module.ts
+++ backend/backend-nest/src/modules/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from "@nestjs/common";
 import { authController } from "./auth.controller";
 import { authService } from "./auth.service";
 import { PrismaService } from "src/database/prisma.service";
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+    imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || "segredo_super_secreto", // ideal usar env
+      signOptions: { expiresIn: "1h" },
+    }),
+  ],
     controllers: [authController],
     providers : [authService, PrismaService],
+    exports: [authService],
 })
 export class authModule{}
+
