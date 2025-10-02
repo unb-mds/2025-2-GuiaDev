c01974feace4b802b6fc27ecde58f027807e6097
filipe brito <filipebritogomesramos@gmail.com>
2025-10-01 23:25:27 -0300

    feature: verificação de login e rota protegida

--- backend/backend-nest/src/modules/auth/auth.module.ts
+++ backend/backend-nest/src/modules/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { authController } from "./auth.controller";
 import { authService } from "./auth.service";
 import { PrismaService } from "src/database/prisma.service";
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
     imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || "segredo_super_secreto", // ideal usar env
       signOptions: { expiresIn: "1h" },
     }),
   ],
     controllers: [authController],
-    providers : [authService, PrismaService],
+    providers : [authService, PrismaService, JwtStrategy],
     exports: [authService],
 })
 export class authModule{}
