aef40d62774a9eb6773c223acd473fa31c47439a
PedroGTG <ph.xavier.exe@gmail.com>
2025-10-03 19:16:36 -0300

    WIP: alterações locais

--- backend/backend-nest/src/app.module.ts
+++ backend/backend-nest/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { GithubModule } from './github/github.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    GithubModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
