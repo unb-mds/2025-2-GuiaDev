607f61c0b018926c61fd237d992f2bcf99e64b96
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-10-14 22:00:22 -0300

    Create Chat.jsx
    chat de IA

---
+++ frontend/src/components/Chat/Chat.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Chat.css';
+
+export default function Chat() {
+
+    return (
+        <div className='chat-box'>
+            <div className='chat-messages'>
+                <p className='chat-placeholder'>GuiaDev Chat Box</p>
+            </div>
+            <div className='chat-input-area'>
+                <input 
+                type="text" 
+                placeholder='Tire sua dúvida!'
+                className='chat-input'
+                />
+
+            </div>
+        </div>
+    );
+}
+
