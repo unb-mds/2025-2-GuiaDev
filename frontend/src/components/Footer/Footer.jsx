c75997a2f0e47d0e486356db3ca05b47fb1cebfd
Hugo Freitas Silva <videogame041@gmail.com>
2025-12-01 11:18:15 -0300

    fix: corrige url da documentação

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
             <span style={{ color: "#fff" }}>
                 &copy; {new Date().getFullYear()} GuiaDev. Desenvolvido para desenvolvedores, por desenvolvedores.
             </span>
-            <a href="/documentacao" style={{ marginLeft: "24px", color: "#fff" }}>Documentação</a>
+            <a href="https://unb-mds.github.io/2025-2-GuiaDev/" style={{ marginLeft: "24px", color: "#fff" }} target='_blank'>Documentação</a>
         </div>
     </footer>
 );
